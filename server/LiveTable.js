e1d9c07e949e22c6773c19596b3ba20ea64ce021
lelanthran <lelanthran@gmail.com>
2022-01-22 00:12:28 +0200

    Added TODOs f what must be done next in LiveTable.

--- server/LiveTable.js
+++ server/LiveTable.js
@@ -11,6 +11,7 @@ function createAttachedElement (elType, parentNode, classList) {
 }
 
 function createColumnHeader (text, parentNode) {
+  // TODO: Add in clickable up/down arrows that call the resort function
   var ret = createAttachedElement ("td", parentNode, "");
   ret.innerHTML = text;
   return ret;
@@ -20,6 +21,8 @@ class LiveTable {
   constructor (dataFunc, recUpdateFunc, recRemoveFunc) {
     this.parentNode = null;
     this.element = null;
+
+    this.divClassList = null;
     this.tableClassList = null;
     this.theadClassList = null;
     this.tbodyClassList = null;
@@ -33,28 +36,36 @@ class LiveTable {
   }
 
   createTableElement () {
+    var div = createAttachedElement ("div", null, this.divClassList);
     var element = createAttachedElement ("table", null, this.tableClassList);
     var thead = createAttachedElement ("thead", element, this.theadClassList);
     var trhead = createAttachedElement ("tr", thead, "");
     var tbody = createAttachedElement ("tbody", element, this.tbodyClassList);
 
+    // TODO: Append table paging controls here: div.appendChild (tblctl);
+
+    // TODO: Append empty cell here (header cell in this column is empty, data
+    // cells have the edit/delete/checkbox elements).
     for (var i=0; i<this.data.table[0].length; i++) {
       var colName = createColumnHeader (this.data.table[0][i], trhead);
       trhead.appendChild (colName);
     }
+    // TODO: Append empty cell here (header cell in this column is empty, data
+    // cells have the edit/delete/checkbox elements).
 
     var ncols = this.data.table[0].length;
     for (var i=1; i<this.data.table.length; i++) {
       var trClassList = (i % 2) == 1 ? this.trOddClassList : this.trEvenClassList;
       var tr = createAttachedElement ("tr", tbody, trClassList);
-      console.log (i + " : Created tr element " + ncols);
+      // TODO: Add in the edit/delete/checkbox elements here
       for (var j=0; j<ncols; j++) {
-        console.log ("Creating td element");
         var td = createAttachedElement ("td", tr, this.tdClassList);
         td.innerHTML = this.data.table[i][j];
       }
+      // TODO: Add in the edit/delete/checkbox elements here
       tbody.appendChild (tr);
     }
+    // TODO: Append table paging controls here: div.appendChild (tblctl);
     return element;
   }
 
